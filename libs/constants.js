d1a2ecf3e424f17768f354a15b9493eabf777960
Shaz Zahra Zaidi <shazzahra.zaidi@gmail.com>
2026-01-10 01:24:15 +0530

    Update constants.js

--- libs/constants.js
+++ libs/constants.js
@@ -1,6 +1,6 @@
 
 export const IS_EDITABLE = window.location.search.includes('edit=true')
-export const OWNER = "YOUR-USERNAME";
+export const OWNER = "Shhazzz";
 export const REPO = "MY-GSOC-TOOL";
-export const EMAIL = "your.email@example.com"
+export const EMAIL = "shazzahra.zaidi@gmail.com"
 export const BRANCH = "main"
